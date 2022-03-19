39772202dc9b22ff5d25f0a27b63dc168f126b38
Revaldo11 <revaldoputra11@gmail.com>
2022-03-19 15:22:22 +0700

    modify chat page

--- lib/screens/home/chat.dart
+++ lib/screens/home/chat.dart
@@ -1,12 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:myshoe/theme.dart';
+import 'package:myshoe/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      elevation: 0,
+      centerTitle: true,
+      automaticallyImplyLeading: false,
+      title: Text(
+        'Message Support',
+        style: primaryTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+      ),
+    );
+  }
+
+  Widget emptyChat() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/image_message.png',
+          width: 80,
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Opss no message yet?',
+          style: primaryTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: medium,
+          ),
+        ),
+        SizedBox(height: 12),
+        Text(
+          'You have never done a transaction',
+          style: secondaryTextStyle,
+        ),
+        SizedBox(height: 20),
+        Container(
+          color: transparantColor,
+          height: 44,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+              backgroundColor: primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            child: Text(
+              'Explore Store',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        color: backgroundColor3,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          children: [
+            ChatTile(),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Chat Page'),
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:myshoe/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  const ChatTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 33),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/icon_store_shoe.png',
+                width: 54,
+              ),
+              SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shoe Store',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Good night, This item is on...',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: light,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Now',
+                style: secondaryTextStyle.copyWith(
+                  fontSize: 10,
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 12),
+          Divider(
+            thickness: 1,
+            color: Color(0xff2B2939),
+          ),
+        ],
+      ),
+    );
+  }
+}
