21cb2bb824f1363487f7f6b55e76b7435d12eec8
Revaldo11 <revaldoputra11@gmail.com>
2022-03-23 20:04:06 +0700

    add navigasi to detail chat screen

--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -6,52 +6,57 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 33),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset(
-                'assets/icon_store_shoe.png',
-                width: 54,
-              ),
-              SizedBox(width: 12),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Shoe Store',
-                      style: primaryTextStyle.copyWith(
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      'Good night, This item is on...',
-                      style: secondaryTextStyle.copyWith(
-                        fontSize: 14,
-                        fontWeight: light,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/detail-chat');
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 33),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/icon_store_shoe.png',
+                  width: 54,
                 ),
-              ),
-              Text(
-                'Now',
-                style: secondaryTextStyle.copyWith(
-                  fontSize: 10,
+                SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Shoe Store',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        'Good night, This item is on...',
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 14,
+                          fontWeight: light,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
-          ),
-          SizedBox(height: 12),
-          Divider(
-            thickness: 1,
-            color: Color(0xff2B2939),
-          ),
-        ],
+                Text(
+                  'Now',
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 10,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 12),
+            Divider(
+              thickness: 1,
+              color: Color(0xff2B2939),
+            ),
+          ],
+        ),
       ),
     );
   }
