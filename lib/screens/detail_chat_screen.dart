892fabe5ebb5096c3778634d0842075dbeaf8d77
Revaldo11 <revaldoputra11@gmail.com>
2022-03-25 20:39:55 -0700

    add chat product Preview

--- lib/screens/detail_chat_screen.dart
+++ lib/screens/detail_chat_screen.dart
@@ -43,31 +43,89 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(bottom: 20),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: priceColor,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoe.png',
+                width: 54,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    overflow: TextOverflow.ellipsis,
+                    style: primaryTextStyle,
+                  ),
+                  SizedBox(height: 2),
+                  Text(
+                    '\$57,15',
+                    style: priceTextStyle,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget chatInput() {
       return Container(
         margin: EdgeInsets.all(20),
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Container(
-                height: 45,
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: backgroundColor4,
-                ),
-                child: Center(
-                  child: TextFormField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Typle Message...',
-                      hintStyle: subtitleTextStyle,
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: backgroundColor4,
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Typle Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                SizedBox(width: 20),
+                Image.asset('assets/button_send.png', width: 45),
+              ],
             ),
-            SizedBox(width: 20),
-            Image.asset('assets/button_send.png', width: 45),
           ],
         ),
       );
