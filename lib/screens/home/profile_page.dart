87ef8ad051e9634c70d22ffa6a1e554347b9f0b8
Revaldo Putra <revaldoputra11@gmail.com>
2022-04-19 16:12:19 +0700

    add header profile page

--- lib/screens/home/profile_page.dart
+++ lib/screens/home/profile_page.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:myshoe/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(defaultMargin),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/image_profile.png',
+                width: 64,
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Valen',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      '@valentyty',
+                      style: subtitleTextStyle.copyWith(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/icon_exit.png',
+                width: 20,
+              ),
+            ],
+          ),
+        )),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: backgroundColor3,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Account',
+                style: primaryTextStyle.copyWith(
+                    fontWeight: semiBold, fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Edit Profile',
+                style: subtitleTextStyle.copyWith(
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
