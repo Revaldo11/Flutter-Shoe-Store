5eee47b862ed038002c5bbca9983b273845d2502
Revaldo Putra <revaldoputra11@gmail.com>
2022-04-30 14:55:08 +0700

    add screen edit profile

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -32,14 +32,131 @@ class EditProfileScreen extends StatelessWidget {
       );
     }
 
+    Widget nameInput() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name',
+              style: secondaryTextStyle.copyWith(
+                fontWeight: reguler,
+                fontSize: 13,
+              ),
+            ),
+            TextField(
+              style: primaryTextStyle,
+              decoration: InputDecoration(
+                hintText: 'Valentino',
+                hintStyle: primaryTextStyle,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: subtitleColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget usernameInput() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Username',
+              style: secondaryTextStyle.copyWith(
+                fontWeight: reguler,
+                fontSize: 13,
+              ),
+            ),
+            TextField(
+              style: primaryTextStyle,
+              decoration: InputDecoration(
+                hintText: '@valentino',
+                hintStyle: primaryTextStyle,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: subtitleColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget emailInput() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Email',
+              style: secondaryTextStyle.copyWith(
+                fontWeight: reguler,
+                fontSize: 13,
+              ),
+            ),
+            TextField(
+              style: primaryTextStyle,
+              decoration: InputDecoration(
+                hintText: 'valen@tino.com',
+                hintStyle: primaryTextStyle,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: subtitleColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget content() {
-      return Container();
+      return Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(
+                top: defaultMargin,
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image_profile.png',
+                  ),
+                ),
+              ),
+            ),
+            nameInput(),
+            usernameInput(),
+            emailInput(),
+          ],
+        ),
+      );
     }
 
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
       body: content(),
+      resizeToAvoidBottomInset: false,
     );
   }
 }
