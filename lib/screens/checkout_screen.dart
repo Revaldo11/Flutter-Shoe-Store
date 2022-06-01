f24a2fe57c89ff0d984d2c3459599fe7bcc049ee
Revaldo Putra <revaldoputra11@gmail.com>
2022-06-01 20:39:42 +0700

    add payment summary & checkout button

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -114,8 +114,136 @@ class CheckoutScreen extends StatelessWidget {
                       ],
                     ),
                   ],
-                )
+                ),
               ],
+            ),
+          ),
+
+          // NOTE: PAYMENT SUMMARY
+          Container(
+            margin: EdgeInsets.only(top: defaultMargin),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: backgroundColor4,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Peyment Summary',
+                  style: primaryTextStyle.copyWith(
+                      fontWeight: medium, fontSize: 16),
+                ),
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Quantity',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                        fontWeight: reguler,
+                      ),
+                    ),
+                    Text(
+                      '2 Items',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Price',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                        fontWeight: reguler,
+                      ),
+                    ),
+                    Text(
+                      '\$575.96',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Shipping',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                        fontWeight: reguler,
+                      ),
+                    ),
+                    Text(
+                      'Free',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 11),
+                Divider(
+                  color: Colors.grey,
+                  thickness: 0.2,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                        fontWeight: reguler,
+                      ),
+                    ),
+                    Text(
+                      '\$575.96',
+                      style: priceTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // NOTE: BUTTON CHECKOUT
+          SizedBox(height: defaultMargin),
+          Divider(
+            color: Colors.grey,
+            thickness: 0.2,
+          ),
+          Container(
+            height: 50,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(vertical: defaultMargin),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Checkout Now',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
             ),
           ),
         ],
