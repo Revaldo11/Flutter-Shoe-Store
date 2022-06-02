356e56051066b1fb3995499e13c77447ddf2bd27
Revaldo Putra <revaldoputra11@gmail.com>
2022-06-02 21:33:27 +0700

    add user model & provider,http package

---
+++ lib/models/users_model.dart
@@ -0,0 +1,39 @@
+class UserModel {
+  late int id;
+  late String name;
+  late String email;
+  late String username;
+  late String profilePhotoUrl;
+  late String token;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.profilePhotoUrl,
+    required this.token,
+  });
+
+  // Constructor from JSON
+  UserModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    username = json['username'];
+    profilePhotoUrl = json['profile_photo_url'];
+    token = json['token'];
+  }
+
+  // Function to convert this object to json
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['email'] = email;
+    data['username'] = username;
+    data['profile_photo_url'] = profilePhotoUrl;
+    data['token'] = token;
+    return data;
+  }
+}
